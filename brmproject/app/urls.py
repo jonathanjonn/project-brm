9e162a515f2f9e74c4e80ac8deea3030a79d0ebd
Jonathan Kent <jonathankent@Jonathans-MacBook-Air.local>
2025-04-09 21:50:07 +0700

    urls pattern name

--- brmproject/app/urls.py
+++ brmproject/app/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import RedirectView
 from app.views import user
 
 urlpatterns = [
-    # path('', RedirectView.as_view(pattern_name='homepage'), name='empty_redirect'),
+    path('', RedirectView.as_view(pattern_name='userList'), name='empty_redirect'),
 
     path('user/', user.userList, name='userList'),
     path('create/user/', user.create, name='create'),
