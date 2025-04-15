bb5614fa58f752d1b1db77949e5c1e8d09c09caa
Jonathan Kent <jonathankent@Jonathans-MacBook-Air.local>
2025-04-15 21:43:57 +0700

    fixing front end

--- brmproject/app/views/auth.py
+++ brmproject/app/views/auth.py
@@ -1,9 +1,9 @@
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth.decorators import user_passes_test
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import user_passes_test, login_required
 from app.form import RegisterForm
 
 @login_required
@@ -12,17 +12,15 @@ def home_view(request):
 
 def login_view(request):
     form = AuthenticationForm(request, data=request.POST or None)
-    message = ''
     if request.method == 'POST':
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect('homepage')  # Ganti sesuai URL tujuan setelah login
+            messages.success(request, 'Login berhasil!')  # Pesan sukses setelah login
+            return redirect('homepage')
         else:
-            message = 'Username atau password salah'
-    return render(request, 'auth/login.html', {'form': form, 'message': message})
-
-
+            messages.error(request, 'Username atau password salah')  # Pesan error jika gagal login
+    return render(request, 'auth/login.html', {'form': form})
 
 @user_passes_test(lambda u: u.is_superuser)
 def register_view(request):
@@ -35,15 +33,16 @@ def register_view(request):
                 password=data['password']
             )
             user.is_superuser = data['is_superuser']
-            user.is_staff = data['is_superuser']  # opsional: buat bisa akses admin panel
+            user.is_staff = data['is_superuser']
             user.save()
-            return redirect('register')  # ganti ke url tujuan setelah register
+            messages.success(request, 'User berhasil didaftarkan!')
+            return redirect('register')  # Ganti dengan URL tujuan setelah register
     else:
         form = RegisterForm()
     return render(request, 'auth/register.html', {'form': form})
 
-
 def logout_view(request):
     if request.method == 'POST':
         logout(request)
+        messages.success(request, 'Logout berhasil!')  # Pesan sukses setelah logout
     return redirect('login')
