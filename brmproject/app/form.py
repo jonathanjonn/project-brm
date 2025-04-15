b33c3d35e0c8e3b8230a8ec23a2aba0966d50dbb
Jonathan Kent <jonathankent@Jonathans-MacBook-Air.local>
2025-04-15 21:19:22 +0700

    user

--- brmproject/app/form.py
+++ brmproject/app/form.py
@@ -1,5 +1,27 @@
 from django import forms
+from django.contrib.auth.models import User
 
 class LoginForm(forms.Form):
-    username = forms.CharField(label='Username', max_length=100)
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(max_length=150, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    is_superuser = forms.BooleanField(required=False, label="Superuser?", widget=forms.CheckboxInput())
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError("Username sudah digunakan.")
+        return username
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm = cleaned_data.get("confirm_password")
+
+        if password and confirm and password != confirm:
+            raise forms.ValidationError("Password dan Konfirmasi tidak cocok.")
--- brmproject/app/views/auth.py
+++ brmproject/app/views/auth.py
@@ -1,14 +1,15 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth.decorators import login_required
+from app.form import RegisterForm
 
 @login_required
 def home_view(request):
     return render(request, 'page/homepage.html')
 
-# LOGIN VIEW
 def login_view(request):
     form = AuthenticationForm(request, data=request.POST or None)
     message = ''
@@ -21,13 +22,24 @@ def login_view(request):
             message = 'Username atau password salah'
     return render(request, 'auth/login.html', {'form': form, 'message': message})
 
-# REGISTER VIEW (hanya untuk superuser)
+
+
 @user_passes_test(lambda u: u.is_superuser)
 def register_view(request):
-    form = UserCreationForm(request.POST or None)
-    if request.method == 'POST' and form.is_valid():
-        form.save()
-        return redirect('login')  # atau ke halaman admin/user list
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = User.objects.create_user(
+                username=data['username'],
+                password=data['password']
+            )
+            user.is_superuser = data['is_superuser']
+            user.is_staff = data['is_superuser']  # opsional: buat bisa akses admin panel
+            user.save()
+            return redirect('register')  # ganti ke url tujuan setelah register
+    else:
+        form = RegisterForm()
     return render(request, 'auth/register.html', {'form': form})
 
 
